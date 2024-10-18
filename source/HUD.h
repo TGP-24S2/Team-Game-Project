b3cd26400b93b1c3a4ef4db33f0257428db837b8
mangorioh <104879428+mangorioh@users.noreply.github.com>
2024-10-18 20:02:01 +1300

    hud data
    gonn keep working on pc

--- source/HUD.h
+++ source/HUD.h
@@ -1,10 +1,13 @@
 #pragma once
 
-// Library includes:
-#include <vector>
-
 #ifndef __HUD_H__
 #define __HUD_H__
+
+// Local includes:
+#include "weapon.h"
+
+// Library includes:
+#include <vector>
 
 //Forward declarations:
 class Renderer;
@@ -24,12 +27,27 @@ public:
 
 	void DebugDraw();
 
+	void SetMaxHealth(int hp);
+	void UpdateCurrentHealth(int hp);
+	void UpdateWeapon(Weapon weapon);
+
 protected:
 
 private:
 
 	// Member data:
 public:
+	Sprite* m_pLevelText;
+	
+	Sprite* m_pHealthIcon;
+	Sprite* m_pHealthText;
+
+	Sprite* m_pWeaponIcon;
+
+	Sprite* m_pMagazineCapacity;
+	weaponType m_eCurrentWeaponType;
+	Sprite* m_pMagazineCount;
+	Sprite* m_pTotalAmmo;
 
 protected:
 
